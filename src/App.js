78b110a74f66e0082495612ee61dbfe11cce3381
mert.alptekin <mert.alptekin@neominal.com>
2022-01-29 12:20:02 +0300

    ReactJS Layout

--- src/App.js
+++ src/App.js
@@ -2,28 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import { Link } from 'react-router-dom';
 // react ile sayfalarımızın birbirine link vasıtası ile yönlenmesi Link component sağlar.
-import { Route, Routes } from 'react-router';
+import { Outlet, Route, Routes } from 'react-router';
 import UsersPage from './pages/UsersPage';
 import TodosPage from './pages/TodosPage';
+import Layout from './components/Layout';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div style={{padding:'20px'}}>
-      <nav>
-        <Link style={{padding:'5px'}} to="users" > Users </Link>
-        <Link style={{padding:'5px'}} to="todos" > Todos </Link>
-      </nav>
-
+    <Container fluid>
       {/* uygulama içerisinde hangi linkin hangi componente yönleneceği kuralını Routes Component içerisinde tanımlıyouruz. */}
       {/* tüm yönledirmeleri Routes altında tutarız, Route ise ilgili link ile sayfaya yönlendiğimiz componentleri tanıttığımız Routing component */}
+
       <Routes>
-        {/* <Route path="/" element={<App />} /> */}
-        <Route path="users" element={<UsersPage />} />
-        <Route path="todos" element={<TodosPage />} />
-        {/* eğer yukarıdaki pathlerden birisi ile eşleşilemediği takdirde aşağıdaki route componente girecektir. */}
-        <Route path="*" element={<div>Sayfa bulunamadı</div>} />
+        <Route path="/" element={<Layout />}>
+          <Route path="users" element={<UsersPage />} />
+          <Route path="todos" element={<TodosPage />} />
+          {/* eğer yukarıdaki pathlerden birisi ile eşleşilemediği takdirde aşağıdaki route componente girecektir. */}
+          <Route path="*" element={<div>Sayfa bulunamadı</div>} />
+        </Route>
       </Routes>
-    </div>
+    </Container>
   );
 }
 
---
+++ src/components/Layout.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import { Card, Container, Nav, Navbar, Button, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router';
+import { Link } from 'react-router-dom';
+// layout bütün sayfalarda değişmeyen tasarımların yapıldığı sayfamız
+// bu sayfa sayesinde sitede üst menü alt menu ve içerik gibi kısımların konumlarını berliliyoruz.
+
+function Layout() {
+    return <div>
+
+
+
+        <Navbar bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Link style={{ padding: '5px' }} to="users" > Users </Link>
+                    <Link style={{ padding: '5px' }} to="todos" > Todos </Link>
+                </Nav>
+            </Container>
+        </Navbar>
+
+ 
+        <Container style={{minHeight:'20rem'}}>
+            <Row>
+                       {/* dinamik olarak syafadan sayfaya yönlendirme ile değişecek olan kısım */}
+                <Outlet />
+            </Row>
+        </Container>
+
+        <Card className="text-center">
+            <Card.Header>Alt Bilgi</Card.Header>
+            <Card.Body>
+                <Card.Title>ReactJS Routing</Card.Title>
+                <Card.Text>
+                   React Routing Uygulaması
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </div>;
+}
+
+export default Layout;
