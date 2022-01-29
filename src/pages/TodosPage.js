793935aa3cd96d829edee8a95e740c74b5d6f702
mert.alptekin <mert.alptekin@neominal.com>
2022-01-29 14:13:51 +0300

    UseRef

--- src/pages/TodosPage.js
+++ src/pages/TodosPage.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Container, ListGroup, Row, Spinner } from 'react-bootstrap';
 
 function TodosPage() {
-  return <div>Todos Pages</div>;
+
+  const [todos, setTodos] = useState([]);
+  const spinnerRef = useRef(); // useRef hook ile elemente document.getElementById gibi bağlanmış oluyoruz. useRef hook bir react elementi yakalamızı onu referansına erişmemiz sağlayan bir yöntem;
+
+  // jquerydeki document ready gibi sayfa ilk açıldığında apiden veri çekebilmemizi sağlayan hook useEffect hook diyoruz.
+  // useEffect(() => {
+
+  //   fetch('https://jsonplaceholder.typicode.com/todos')
+  //   .then(response => {
+  //     return response.json();
+  //   }).then(data => {
+  //     console.log('data', data);
+  //     setTodos(data); // todos state data bilgisini jsx'e taşıdık.
+  //   });
+
+  // }, []);
+
+  useEffect(async () => {
+
+    // ECMAScript 7.0 verisyonu ile Promise yapısı then catch yapısı yerine async await yapısı gelid yukarıdaki kod ile tümüyşle aynı işi yapar.
+    // show loading 
+
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    let data = await response.json();
+    setTodos(data);
+    // spinnerRef.current ile elemente erişi sanki jquery yada js ile kodluyormuş gibi attribute ekleye çıkarabilir class verip kaldırabiliriz.
+    spinnerRef.current.setAttribute("hidden","hidden") // gizle
+
+   
+    // hide loading
+
+  }, [])
+
+
+  return <Container>
+    <Row className="justify-content-center">
+    <Spinner ref={spinnerRef} animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
+    <ListGroup>
+      {
+        todos.map((item, key) => {
+          return <ListGroup.Item key={key}>{item.title}</ListGroup.Item>
+        })
+      }
+    </ListGroup>
+    </Row>
+  </Container>;
 }
 
 export default TodosPage;
-    
--- src/pages/UserDetailPage.js
+++ src/pages/UserDetailPage.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import { UserData } from './UsersData';
+// bu hook ile dinamik paramtereleri yani id den gönderilen değeri yakalıyorum
+// hook demek uygulamaya bir özellik kazandıran yaşam döngüsü elemanı
+// urldeki dinamik parametreleri okumamızı sağlar
 
 function UserDetailPage() {
-  return <div>User Detail</div>;
+
+  const param = useParams();
+
+  const user = UserData.find(x=> x.id == param.id);
+
+  console.log('param', user);
+
+  return <div>
+    <p>UserName: {user.name} {user.surname}</p>
+  </div>;
 }
 
 export default UserDetailPage;
